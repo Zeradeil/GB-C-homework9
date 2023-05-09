fa7d8511722fe276d1d668bc9abc7adfec25be36
Zeradeil <wandark@ya.ru>
2023-05-09 13:10:52 +0700

    решено 1

--- task1/Program.cs
+++ task1/Program.cs
@@ -3,3 +3,29 @@
 // M = 1; N = 5 -> "2, 4"
 // M = 4; N = 8 -> "4, 6, 8"
 
+int M = Readnum("начало");
+int N = Readnum("конец");
+NaturalNumbers(M, N);
+
+
+int Readnum(string massage)
+{
+    System.Console.Write($"Введите {massage} промежутка => ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void NaturalNumbers(int M, int N)
+{
+    if (M % 2 != 0)
+    {
+        M += 1;
+    }
+    if (N >= M)
+    {
+        System.Console.Write($"{M} ");
+        M += 2;
+        NaturalNumbers(M, N);
+    }
+    return;
+}
+
