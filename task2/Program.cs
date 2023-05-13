d040df38a2fd882de626a4c65ef20b2f36d6a103
Zeradeil <wandark@ya.ru>
2023-05-13 11:55:27 +0700

    решено 3

--- task2/Program.cs
+++ task2/Program.cs
@@ -3,4 +3,34 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8 -> 30
 
+int M = Readnum("начало");
+int N = Readnum("конец");
+int sum = 0;
+System.Console.WriteLine(SumNumbers(M, N, sum));
+
+
+int Readnum(string massage)
+{
+    System.Console.Write($"Введите {massage} промежутка => ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumNumbers(int M, int N, int sum)
+{
+
+    if (M % 2 != 0)
+    {
+        M += 1;
+    }
+    if (N < M)
+    {
+        return 0;
+    }
+    sum += M;
+    System.Console.Write($"{M} ");
+    M += 2;
+    SumNumbers(M, N, sum);
+
+    return sum += M;
+}
 
--- task3/Program.cs
+++ task3/Program.cs
@@ -2,4 +2,30 @@
 //  Даны два неотрицательных числа m и n. 
 // m = 3, n = 2 -> A(m,n) = 29
 
+int m =  Readnum("M");
+int n = Readnum("N");
+int ack = Ackermann(m, n);
+System.Console.WriteLine(ack);
 
+int Readnum(string massage)
+{
+    System.Console.Write($"Введите {massage} => ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Ackermann(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    if (n == 0 && m > 0)
+    {
+        return Ackermann(m - 1, 1);
+    }
+    if (m > 0 && n > 0)
+    {
+        return Ackermann(m - 1, Ackermann(m, n - 1));
+    }
+    return Ackermann(m, n);
+}
